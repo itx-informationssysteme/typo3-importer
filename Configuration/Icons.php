c674932ac9174b82992a359fb32409d688990488
Benjamin Jasper <benjamin.jasper@itx.de>
2024-09-25 15:28:17 +0200

    fix: fixed icon issue

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -4,6 +4,6 @@ return [
     'importer-logo' => [
         'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         // the source bitmap file
-        'source' => 'EXT:typo3_importer/Resources/Public/Icons/logo.svg',
+        'source' => 'EXT:importer/Resources/Public/Icons/logo.svg',
     ],
 ];
