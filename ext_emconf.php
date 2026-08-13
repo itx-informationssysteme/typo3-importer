22639775af6cef93e2e942c38e41b3c687d2e58b
Patrik Marosváry <128425620+paeddym@users.noreply.github.com>
2026-08-13 14:40:41 +0200

    release: 2.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -28,5 +28,5 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => 1,
     'author' => 'it.x informationssysteme gmbh',
     'author_email' => '',
-    'version' => '2.0.0',
+    'version' => '2.0.1',
 ];
